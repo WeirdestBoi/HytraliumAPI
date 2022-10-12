24a29153b77b1e09dd641d67215c4207c5d7ccbb
BuildTools <unconfigured@null.spigotmc.org>
2022-10-12 18:03:00 +0200

    added object converter

---
+++ src/main/java/me/hytralium/hytraliumapi/ObjectConverter.java
@@ -0,0 +1,41 @@
+package me.hytralium.hytraliumapi;
+
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+public class ObjectConverter {
+    public String convertObjectToString(Object object) {
+        String encodedOnject;
+        try {
+            ByteArrayOutputStream io = new ByteArrayOutputStream();
+            BukkitObjectOutputStream os = new BukkitObjectOutputStream(io);
+            os.writeObject(object);
+            os.flush();
+            byte[] serializedObject = io.toByteArray();
+            encodedOnject = Base64.getEncoder().encodeToString(serializedObject);
+            return encodedOnject;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object convertStringToObject(String string) {
+        try {
+            byte[] serializedObject;
+            serializedObject = Base64.getDecoder().decode(string);
+            ByteArrayInputStream in = new ByteArrayInputStream(serializedObject);
+            BukkitObjectInputStream is = new BukkitObjectInputStream(in);
+            return is.readObject();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
